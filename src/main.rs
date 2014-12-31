39f25236d9b3897cb7d449bbf84ffd42c6c540c9
Antti Keränen <detegr@gmail.com>
2014-12-31 14:42:33 +0200

    Docopt command line parsing
    Command line flags that can be used to tell cargo-watch which cargo commands to run
    when files change.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 #![warn(missing_docs)]
 //! Watch files in a Cargo project and compile it when they change
 
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate docopt;
+#[phase(plugin)] extern crate docopt_macros;
+
 extern crate notify;
 #[phase(plugin, link)] extern crate log;
 
@@ -12,6 +16,20 @@ mod cargo;
 mod compile;
 mod ignore;
 mod timelock;
+
+docopt!(Args deriving Show, "
+Usage: cargo-watch [options]
+       cargo watch [options]
+
+Options:
+  -h, --help      Display this message
+  -b, --build     Run `cargo build` when a file is modified
+  -d, --doc       Run `cargo doc` when a file is modified
+  -t, --test      Run `cargo test` when a file is modified
+  -n, --bench     Run `cargo bench` when a file is modified
+
+Default options are `build` and `test`
+");
 
 fn main() {
   let (tx, rx) = channel();
