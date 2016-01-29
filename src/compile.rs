f65af1b5419c62ceb2025875570457d8f4cc9fcd
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-01-29 18:06:23 +0100

    Some cleanup in `compile.rs`

--- src/compile.rs
+++ src/compile.rs
@@ -4,26 +4,33 @@ use std::thread;
 use super::{Config, cargo, ignore, notify, timelock};
 
 fn compile(t: Arc<AtomicIsize>, c: Arc<Config>) {
-    let Config {
-    ref args
-  } = *c;
+    let Config { ref args } = *c;
     debug!("Starting a compile");
-    if args.len() > 0 {
-        args.iter().map(|v| cargo::run(v)).last();
+
+    // TODO: This still has allocation overhead -> remove it
+    let commands = if args.len() > 0 {
+        args.iter().map(|s| &s[..]).collect()
     } else {
-        vec![String::from("build"), String::from("test")].iter().map(|v| cargo::run(v)).last();
+        vec!["build", "test"]
+    };
+
+    for command in commands {
+        cargo::run(command);
     }
+
     timelock::update(&t);
     debug!("Compile done");
 }
 
 fn spawn_compile(t: &Arc<AtomicIsize>, c: Arc<Config>) {
     info!("Request to spawn a compile");
+
     // Don't run compiles within less than 2s of each other
+    // TODO: put this constant somewhere else
     let justnow = timelock::current() - 2;
     let prev = timelock::get(t);
     if prev > justnow {
-        info!("Request denied");
+        info!("Request denied (last request was less than 2 seconds ago)");
     } else {
         timelock::update(t);
         let t2 = t.clone();
@@ -34,20 +41,17 @@ fn spawn_compile(t: &Arc<AtomicIsize>, c: Arc<Config>) {
 }
 
 pub fn handle_event(t: &Arc<AtomicIsize>, e: notify::Event, c: Arc<Config>) {
-    match e.path {
-        None => return,
-        Some(p) => {
-            debug!("path: {}", p.display());
-            match p.file_name() {
-                None => return,
-                Some(f) => {
-                    let name = f.to_string_lossy();
-                    if ignore::filename(&name) {
-                        info!("Ignoring change on '{}' ({})", name, p.display());
-                    } else {
-                        spawn_compile(t, c);
-                    }
-                }
+    // Check if the event refers to a valid file
+    if let Some(path) = e.path {
+        debug!("path: {}", path.display());
+
+        if let Some(filename) = path.file_name() {
+            // Check if this file should be ignored
+            let filename = filename.to_string_lossy();
+            if ignore::filename(&filename) {
+                info!("Ignoring change on '{}' ({})", filename, path.display());
+            } else {
+                spawn_compile(t, c);
             }
         }
     }
