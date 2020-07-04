08695ba5234e1168c87828090b7292b5df179153
Félix Saparelli <felix@passcod.name>
2020-07-04 18:06:11 +1200

    Upgrade to watchexec 1.14

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,11 @@
 //! Watch files in a Cargo project and compile it when they change
-#![forbid(unsafe_code, clippy::pedantic)]
-#![allow(
-    clippy::non_ascii_literal,
-    clippy::cast_sign_loss,
-    clippy::cast_possible_truncation,
-    clippy::too_many_lines
-)]
+#![warn(clippy::all)]
 
 #[macro_use]
 extern crate clap;
-extern crate watchexec;
 
 use clap::{ArgMatches, Error, ErrorKind};
+use log::debug;
 use std::{env::set_current_dir, path::MAIN_SEPARATOR};
 use watchexec::{Args, ArgsBuilder};
 
@@ -27,7 +21,7 @@ pub fn change_dir() {
         });
 }
 
-pub fn set_commands(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches) {
+pub fn set_commands(builder: &mut ArgsBuilder, matches: &ArgMatches) {
     let mut commands: Vec<String> = Vec::new();
 
     // Cargo commands are in front of the rest
@@ -51,18 +45,13 @@ pub fn set_commands(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches
         commands.push("cargo check".into());
     }
 
-    if debug {
-        println!(">>> Commands: {:?}", commands);
-    }
-
+    debug!("Commands: {:?}", commands);
     builder.cmd(commands);
 }
 
-pub fn set_ignores(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches) {
+pub fn set_ignores(builder: &mut ArgsBuilder, matches: &ArgMatches) {
     if matches.is_present("ignore-nothing") {
-        if debug {
-            println!(">>> Ignoring nothing");
-        }
+        debug!("Ignoring nothing");
 
         builder.no_vcs_ignore(true);
         builder.no_ignore(true);
@@ -71,15 +60,11 @@ pub fn set_ignores(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches)
 
     let novcs = matches.is_present("no-gitignore");
     builder.no_vcs_ignore(novcs);
-    if debug {
-        println!(">>> Load Git/VCS ignores: {:?}", !novcs);
-    }
+    debug!("Load Git/VCS ignores: {:?}", !novcs);
 
     let noignore = matches.is_present("no-ignore");
     builder.no_ignore(noignore);
-    if debug {
-        println!(">>> Load .ignore ignores: {:?}", !noignore);
-    }
+    debug!("Load .ignore ignores: {:?}", !noignore);
 
     let mut list = vec![
         // Mac
@@ -104,9 +89,7 @@ pub fn set_ignores(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches)
         format!("*{s}target{s}**", s = MAIN_SEPARATOR),
     ];
 
-    if debug {
-        println!(">>> Default ignores: {:?}", list);
-    }
+    debug!("Default ignores: {:?}", list);
 
     if matches.is_present("ignore") {
         for ignore in values_t!(matches, "ignore", String).unwrap_or_else(|e| e.exit()) {
@@ -116,19 +99,15 @@ pub fn set_ignores(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches)
         }
     }
 
-    if debug {
-        println!(">>> All ignores: {:?}", list);
-    }
-
+    debug!("All ignores: {:?}", list);
     builder.ignores(list);
 }
 
-pub fn set_debounce(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches) {
+pub fn set_debounce(builder: &mut ArgsBuilder, matches: &ArgMatches) {
     let d = if matches.is_present("delay") {
         let debounce = value_t!(matches, "delay", f32).unwrap_or_else(|e| e.exit());
-        if debug {
-            println!(">>> File updates debounce: {} seconds", debounce);
-        }
+        debug!("File updates debounce: {} seconds", debounce);
+
         (debounce * 1000.0) as u32
     } else {
         500
@@ -137,7 +116,7 @@ pub fn set_debounce(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches
     builder.poll_interval(d).debounce(d);
 }
 
-pub fn set_watches(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches) {
+pub fn set_watches(builder: &mut ArgsBuilder, matches: &ArgMatches) {
     let mut opts = Vec::new();
     if matches.is_present("watch") {
         for watch in values_t!(matches, "watch", String).unwrap_or_else(|e| e.exit()) {
@@ -149,34 +128,28 @@ pub fn set_watches(debug: bool, builder: &mut ArgsBuilder, matches: &ArgMatches)
         opts.push(".".into());
     }
 
-    if debug {
-        println!(">>> Watches: {:?}", opts);
-    }
-
+    debug!("Watches: {:?}", opts);
     builder.paths(opts);
 }
 
-pub fn get_options(debug: bool, matches: &ArgMatches) -> Args {
+pub fn get_options(matches: &ArgMatches) -> Args {
     let mut builder = ArgsBuilder::default();
     builder
         .restart(!matches.is_present("no-restart"))
         .poll(matches.is_present("poll"))
         .clear_screen(matches.is_present("clear"))
         .watch_when_idle(matches.is_present("watch-when-idle"))
-        .debug(debug)
-        .run_initially(!matches.is_present("postpone"));
+        .run_initially(!matches.is_present("postpone"))
+        .no_environment(true);
 
-    set_ignores(debug, &mut builder, &matches);
-    set_debounce(debug, &mut builder, &matches);
-    set_watches(debug, &mut builder, &matches);
-    set_commands(debug, &mut builder, &matches);
+    set_ignores(&mut builder, &matches);
+    set_debounce(&mut builder, &matches);
+    set_watches(&mut builder, &matches);
+    set_commands(&mut builder, &matches);
 
     let mut args = builder.build().unwrap();
     args.once = matches.is_present("once");
 
-    if debug {
-        println!(">>> Watchexec arguments: {:?}", args);
-    }
-
+    debug!("Watchexec arguments: {:?}", args);
     args
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 //! Watch files in a Cargo project and compile it when they change
-#![forbid(unsafe_code, clippy::pedantic)]
+#![warn(clippy::all)]
 
-extern crate cargo_watch;
-extern crate watchexec;
+use cargo_watch::{args, change_dir, watch::CwHandler, get_options};
+use watchexec::{error::Result, run::watch};
 
-fn main() -> watchexec::error::Result<()> {
-    let matches = cargo_watch::args::parse();
-
-    cargo_watch::change_dir();
+fn main() -> Result<()> {
+    let matches = args::parse();
+    change_dir();
 
     let quiet = matches.is_present("quiet");
     let debug = matches.is_present("debug");
-    let opts = cargo_watch::get_options(debug, &matches);
-    let handler = cargo_watch::watch::CwHandler::new(opts, quiet)?;
-    watchexec::run::watch(&handler)
+
+    stderrlog::new()
+        .quiet(quiet)
+        .verbosity(if debug { 3 } else { 1 })
+        .timestamp(stderrlog::Timestamp::Millisecond)
+        .init()
+        .unwrap();
+
+    let opts = get_options(&matches);
+    let handler = CwHandler::new(opts, quiet)?;
+    watch(&handler)
 }
