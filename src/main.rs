2179fde8e68d8a1690aa55296f91a4c43a4b1ea1
Félix Saparelli <felix@passcod.name>
2020-07-04 19:14:02 +1200

    Update test snapshots

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,21 @@
 #![warn(clippy::all)]
 
 use cargo_watch::{args, change_dir, watch::CwHandler, get_options};
+use stderrlog::Timestamp;
 use watchexec::{error::Result, run::watch};
 
 fn main() -> Result<()> {
     let matches = args::parse();
     change_dir();
 
-    let quiet = matches.is_present("quiet");
     let debug = matches.is_present("debug");
+    let quiet = matches.is_present("quiet");
+    let testing = matches.is_present("once");
 
     stderrlog::new()
         .quiet(quiet)
         .verbosity(if debug { 3 } else { 1 })
-        .timestamp(stderrlog::Timestamp::Millisecond)
+        .timestamp(if testing { Timestamp::Off } else { Timestamp::Millisecond })
         .init()
         .unwrap();
 
