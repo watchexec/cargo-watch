1c1fb4d8ad85246c28e9c479a396198f5672beb2
Félix Saparelli <felix@passcod.name>
2022-07-09 23:22:03 +1200

    Add testing utility

---
+++ examples/test-cw.rs
@@ -0,0 +1,94 @@
+use std::{
+	env,
+	fs::write,
+	path::PathBuf,
+	thread::sleep,
+	time::{Duration, Instant},
+};
+
+use clap::Parser;
+use duct::cmd;
+use miette::{bail, IntoDiagnostic, Result};
+use tempfile::TempDir;
+
+#[derive(Debug, Parser)]
+struct Args {
+	#[clap(long)]
+	vcs: Option<String>,
+
+	#[clap(long)]
+	bin: bool,
+
+	#[clap(long, multiple_occurrences = true)]
+	touch: Vec<PathBuf>,
+
+	#[clap(long, default_value = "200")]
+	before: u64,
+
+	#[clap(long, default_value = "200")]
+	between: u64,
+
+	#[clap(long, default_value = "5000")]
+	timeout: u64,
+
+	#[clap(raw = true)]
+	cmd: Vec<String>,
+}
+
+fn main() -> Result<()> {
+	let mut args = Args::parse();
+
+	let tmp_dir = TempDir::new_in(".").into_diagnostic()?;
+
+	let mut init_args = vec!["init", "--quiet", "--name", "cw-test", "--offline"];
+	if args.bin {
+		init_args.push("--bin");
+	}
+	if let Some(vcs) = &args.vcs {
+		init_args.push("--vcs");
+		init_args.push(vcs);
+	}
+	cmd("cargo", init_args)
+		.dir(tmp_dir.path())
+		.run()
+		.into_diagnostic()?;
+
+	let before = Duration::from_millis(args.before);
+	let between = Duration::from_millis(args.between);
+	let timeout = Duration::from_millis(args.timeout);
+
+	args.cmd.insert(0, "watch".into());
+	let cw = cmd(
+		PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target/debug/cargo-watch"),
+		args.cmd,
+	)
+	.dir(tmp_dir.path())
+	.start()
+	.into_diagnostic()?;
+
+	sleep(before);
+
+	for file in args.touch {
+		write(tmp_dir.path().join(file), "something").into_diagnostic()?;
+		sleep(between);
+	}
+
+	let start = Instant::now();
+	let mut good = false;
+	while start.elapsed() < timeout {
+		if cw.try_wait().into_diagnostic()?.is_some() {
+			good = true;
+			break;
+		} else {
+			sleep(Duration::from_millis(10));
+		}
+	}
+
+	tmp_dir.close().into_diagnostic()?;
+
+	if !good {
+		bail!("Timed out");
+	}
+
+	Ok(())
+}
--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,44 +1,18 @@
-use std::{
-	env::{current_dir, set_current_dir},
-	path::PathBuf,
-};
+use std::{env, path::PathBuf};
 
-use duct::cmd;
-use miette::{IntoDiagnostic, Result};
-use tempfile::TempDir;
-use trycmd::{cargo::cargo_bin, TestCases};
-
-fn prepare() -> Result<(PathBuf, TempDir)> {
-	let tests_dir = current_dir()
-		.into_diagnostic()?
-		.join("tests")
-		.join("trycmd");
-
-	let tmp_dir = TempDir::new().into_diagnostic()?;
-	set_current_dir(tmp_dir.path()).into_diagnostic()?;
-	cmd!(
-		"cargo",
-		"init",
-		"--vcs",
-		"git",
-		"--bin",
-		"--name",
-		"cw-test",
-		"--offline"
-	)
-	.run()
-	.into_diagnostic()?;
-
-	Ok((tests_dir, tmp_dir))
-}
+use trycmd::{cargo::cargo_bin, schema::Bin, TestCases};
 
 #[test]
 #[cfg(unix)]
-fn unix_tests() -> Result<()> {
-	let (tests_dir, tmp_dir) = prepare()?;
+fn unix_tests() {
 	TestCases::new()
 		.default_bin_path(cargo_bin!("cargo-watch"))
-		.case(tests_dir.join("unix.trycmd"));
-	tmp_dir.close().into_diagnostic()?;
-	Ok(())
+		.register_bin(
+			"test-cw",
+			Bin::Path(
+				PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+					.join("target/debug/examples/test-cw"),
+			),
+		)
+		.case("tests/trycmd/unix/*.trycmd");
 }
